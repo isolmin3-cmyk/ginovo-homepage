ece0b395154393864440fccaac361bd1039af5b3
isolmin3-cmyk <isolmin3@gmail.com>
2026-08-16 21:00:41 +0900

    Add editable smart golf ball media slots

---
+++ assets/smartball-media-config.js
@@ -0,0 +1,19 @@
+/*
+ * 관리자 페이지 연동 지점입니다.
+ * 업로드된 이미지 URL을 각 슬롯 ID의 값으로 저장하면 공개 페이지에 반영됩니다.
+ * 빈 값은 현재 합성 디자인 이미지를 그대로 보여줍니다.
+ */
+window.SMARTBALL_MEDIA = {
+  'spec-weight': '',
+  'spec-size': '',
+  'spec-rebound': '',
+  'spec-eccentricity': '',
+  'putting-system-set': '',
+  'wireless-charger': '',
+  'distance-practice-screen': '',
+  'distance-analysis-screen': '',
+  'slope-practice-screen': '',
+  'slope-selection-screen': '',
+  'battle-player-a': '',
+  'battle-player-b': ''
+};
