f9e9b0039ad829b77e64ab49a39e2d8567b7c279
isolmin3-cmyk <isolmin3@gmail.com>
2026-08-17 02:18:47 +0900

    Rebuild editable main homepage sections

---
+++ assets/home-content-config.js
@@ -0,0 +1,22 @@
+window.HOME_CONTENT = {
+  section1Eyebrow: '지노버 스마트 골프공',
+  section1Title: '골프공의 한계를 넘어,\n골프 분석의 새로운 표준이 되다.',
+  section1Body: '독창성, 우수성, 신뢰성, 파급효과 -\nNEP 신제품인증의 모든 기준을 완벽히 충족한\n혁신의 결정체.',
+  section2Title: 'Unity 기반의 물리엔진을 통한 스크린 골프 구현',
+  section2Body: '퍼팅 샷에 대한 7대 구질 정보 가시화\n블루투스 통신을 통한 샷 정보 실시간 가시화',
+  section3Title: '감에 의존하는 지루한 연습에서 데이터를 기반으로 한 체계적 연습',
+  section3Body: '목표 거리 설정 - 통계분석 - 반복 연습 - 퍼팅 대결 실전 연습',
+  section4Title: '지노버 경사 퍼팅매트'
+};
+
+window.HOME_MEDIA = {
+  section1Background: './assets/home/section-1-background.jpg',
+  section2Background: './assets/home/section-2-background.jpg',
+  section2Visual: './assets/home/section-2-visual.png',
+  section3Background: './assets/home/section-3-background.jpg',
+  section3Visual: './assets/home/section-3-visual.png',
+  section4Background: './assets/home/section-4-background.jpg',
+  section4Image1: './assets/putting-slope.png',
+  section4Image2: './assets/home/putting-course.jpg.png',
+  section4Image3: './assets/home/putting-kit.jpg.png'
+};
