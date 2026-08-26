a50627de66f3ad71a6c6e73c1dd7ac3eb7d68364
isolmin3-cmyk <isolmin3@gmail.com>
2026-08-26 14:38:26 +0900

    Update homepage smart golf solution copy

--- assets/home-content-config.js
+++ assets/home-content-config.js
@@ -1,9 +1,9 @@
 window.HOME_CONTENT = {
   section1Eyebrow: '지노버 스마트 골프공',
   section1Title: '골프공의 한계를 넘어,\n골프 분석의 새로운 표준이 되다.',
-  section1Body: '독창성, 우수성, 신뢰성, 파급효과 -\nNEP 신제품인증의 모든 기준을 완벽히 충족한\n혁신의 결정체.',
-  section2Title: 'Unity 기반의 물리엔진을 통한 스크린 골프 구현',
-  section2Body: '퍼팅 샷에 대한 7대 구질 정보 가시화\n블루투스 통신을 통한 샷 정보 실시간 가시화',
+  section1Body: '독창성, 우수성, 신뢰성, 파급효과 새로운 골프 분석의 기준을 제시하는 혁신적인 스마트 골프 솔루션',
+  section2Title: 'Unity 기반의 물리엔진을 통한 골프혁신',
+  section2Body: '퍼팅 샷에 대한 7대 구질 정보 가시화\n블루투스 통신을 통한 샷 정보 실시간 전송',
   section3Title: '감에 의존하는 지루한 연습에서 데이터를 기반으로 한 체계적 연습',
   section3Body: '목표 거리 설정 - 통계분석 - 반복 연습 - 퍼팅 대결 실전 연습',
   section4Title: '지노버 경사 퍼팅매트'
